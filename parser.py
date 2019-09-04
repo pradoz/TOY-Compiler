f2c09f78d109b93dab5bd2333cfc83fb7eab39c5
znmop <zacharyprado1@gmail.com>
2019-09-03 19:30:05 -0700

    use \'python input.toy\' to get output to stdout (temporary). llc next so we can compile the object file into an executable

--- parser.py
+++ parser.py
@@ -1,27 +1,14 @@
-'''
-Parsing, syntax analysis, or syntactic analysis is the process of analysing a
-string of symbols, either in natural language, computer languages or data
-structures, conforming to the rules of a formal grammar. The term parsing
-comes from Latin pars (orationis), meaning part (of speech).
-[https://en.wikipedia.org/wiki/Parsing]
-'''
-
 from rply import ParserGenerator
 from ast import Number, Sum, Sub, Print
 
 
 class Parser():
-    def __init__(self):
-        # List of tokens accepted by the parser
-        self.pg = ParserGenerator([   
-                  'NUMBER',
-                  'PRINT',
-                  'OPEN_PAREN',
-                  'CLOSE_PAREN',
-                  'SEMI_COLON',
-                  'SUM',
-                  'SUB'
-                ])
+    def __init__(self, module, builder, printf):
+        self.pg = ParserGenerator(
+            # List containing all token names accepted by the parser
+            ['NUMBER', 'PRINT', 'OPEN_PAREN', 'CLOSE_PAREN',
+             'SEMI_COLON', 'SUM', 'SUB']
+        )
         self.module = module
         self.builder = builder
         self.printf = printf
@@ -35,8 +22,8 @@ class Parser():
         @self.pg.production('expression : expression SUB expression')
         def expression(p):
             left = p[0]
-            operator = p[1]
             right = p[2]
+            operator = p[1]
             if operator.gettokentype() == 'SUM':
                 return Sum(self.builder, self.module, left, right)
             elif operator.gettokentype() == 'SUB':
